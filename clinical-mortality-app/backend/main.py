ee8221068ec9b4455802ad6679399dec88eddf67
R-One <erwann.lesech@epita.fr>
2025-09-30 20:28:33 +0200

    fix<ci>: fix python import sorting

--- clinical-mortality-app/backend/main.py
+++ clinical-mortality-app/backend/main.py
@@ -1,9 +1,10 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import httpx
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
